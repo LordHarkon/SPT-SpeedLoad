11f56a2050e06679f085fca1ced7e455afb18644
Neata Alexandru <frez758@gmail.com>
2024-07-09 21:39:38 +0300

    Updated to SPT 3.9.x

--- src/mod.ts
+++ src/mod.ts
@@ -1,29 +1,32 @@
 import { DependencyContainer } from "tsyringe";
 
-import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
-import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
-import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
-import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
-import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
+import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
+import { DatabaseService } from "@spt/services/DatabaseService";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
+import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
 
-class Mod implements IPostDBLoadMod {
-    private modConfig = require("../config.json");
+class Mod implements IPostDBLoadMod
+{
+    private configuration = require("../config.json");
 
-    public postDBLoad(container: DependencyContainer): void {
+    public postDBLoad(container: DependencyContainer): void
+    {
         // get database from server
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const databaseService = container.resolve<DatabaseService>("DatabaseService");
         const logger = container.resolve<ILogger>("WinstonLogger");
 
         // Get all the in-memory json found in /assets/database
-        const tables: IDatabaseTables = databaseServer.getTables();
+        const tables: IDatabaseTables = databaseService.getTables();
 
         const globals = tables.globals;
 
         // Change the base loading time of the magazines
-        globals.config.BaseLoadTime = this.modConfig.baseLoadTime; // vanilla default: 0.85
+        globals.config.BaseLoadTime = this.configuration.baseLoadTime; // vanilla default: 0.85
         // Change the base unloading time of the magazines
-        globals.config.BaseUnloadTime = this.modConfig.baseUnloadTime; // vanilla default: 0.3
+        globals.config.BaseUnloadTime = this.configuration.baseUnloadTime; // vanilla default: 0.3
 
+        // Log the changes
         logger.logWithColor(
             "Changed base loading time of magazines: " + globals.config.BaseLoadTime,
             LogTextColor.GREEN
@@ -35,4 +38,4 @@ class Mod implements IPostDBLoadMod {
     }
 }
 
-module.exports = { mod: new Mod() };
+export const mod = new Mod();
